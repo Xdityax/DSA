464509ac9a81fa735258aeca8dadfa8ce53d8e3a
Aditya-6969 <165007421+Aditya-6969@users.noreply.github.com>
2026-02-17 09:51:31 +0530

    dsa

--- matrixprint.py
+++ matrixprint.py
@@ -1,6 +1,30 @@
-n=int(input("Enter the size of the matrix: "))
+'''n=int(input("Enter the size of the matrix: "))
 m=int(input("Enter the number of columns: "))
 for i in range(n):
     for j in range(1,m+1):
         print(j, end=" ")
-    print()
+    print()'''
+
+
+'''
+n=int(input("Enter the size of the matrix: "))
+for i in range(n):
+    for j in range(1,n+1):
+        if i<=j-1:
+            print(j, end=" ")
+        else:
+            print(" ", end=" ")
+    print() 
+
+    
+''''''
+n=int(input("Enter the size of the matrix: "))
+for i in range(n):
+    for j in range(n):
+        if j<=i:
+            print(j+1,end=" ")
+        else:
+            print("",end=" ")
+    print(" ")
+'''
+
